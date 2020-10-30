e33f04fa63c1721f78540b176b9baf583be832d3
ripper <453942056@qq.com>
2020-10-30 11:16:39 +0800

    修改 id

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 DOWNLOAD_URL=https://raw.githubusercontent.com/ripperhe/XcodeWay/master/XcodeWayExtensions/Script/XcodeWayScript.scpt
-SCRIPT_DIR="${HOME}/Library/Application Scripts/com.fantageek.XcodeWayApp.XcodeWayExtensions"
+SCRIPT_DIR="${HOME}/Library/Application Scripts/com.ripperhe.XcodeWayApp.XcodeWayExtensions"
 
 mkdir -p "${SCRIPT_DIR}"
 curl $DOWNLOAD_URL -o "${SCRIPT_DIR}/XcodeWayScript.scpt"
